0a1365a8e61459c7cdbbb61070f4bf24bd69cdd0
diego <diego.falciola@gmail.com>
2015-06-13 07:50:04 -0300

    Api methods definition

--- WEB-INF/includes/classes/Api.php
+++ WEB-INF/includes/classes/Api.php
@@ -1,16 +1,30 @@
 <?php
 
+require_once "WEB-INF/includes/classes/class.x3.database.php";
+require_once "WEB-INF/includes/config.php";
+require_once "WEB-INF/includes/functions/functions_utils.php";
 
 class Api {
     var $resellerId;
     var $resellerToken;
+
+    var $db;
 
     function Api($resellerId, $resellerToken)
     {
         $this->resellerId= $resellerId;
         $this->resellerToken = $resellerToken;
 
+        $this->db = new DB_config;
+        $this->db->connect();
 
+
+        $query = "SELECT reseller_id FROM `".$this->db->resellers."` WHERE reseller_id=" . $resellerId;
+        $reseller_ids = $this->db->select_field($this->db->resellers, "reseller_id", "", $query);
+
+        if (empty($reseller_ids)){
+            die("empty");
+        }
 
     }
 
