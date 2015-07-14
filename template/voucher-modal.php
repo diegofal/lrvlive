3b328eb71d24dfb6c30ca53e8ee8ba18136a44e7
César Pantuso <cesar.pantuso@gmail.com>
2015-07-14 09:11:28 -0300

    Add new files to template

---
+++ template/voucher-modal.php
@@ -0,0 +1,195 @@
+<div class="md-modal md-effect-11" id="modal-2">
+    <div class="md-content">
+        <section id="voucher">
+                    <div class="breadcrum-step">
+                        <a href="#" class="selected">1. Voucher details</a>
+                        <a href="#">2. Ticket type</a>
+                        <a href="#">3. Resume and payment</a>
+                    </div>
+                    <div class="step step-1">
+                        <header>
+                            <span>Step 1</span>
+                            <h3>Vouchers</h3>
+                            <p>Please enter the following details to purchase your Voucher</p>
+                        </header>
+                        <div class="buy-content">
+                            <ul>
+                                <li><span>Name of person to receive this Voucher:</span><input type="text" /></li>
+                                <li><span>Phone number of receiver:</span><input type="text"/></li>
+                                <li><span>Email of sender:</span><input type="text"/></li>
+                                <li><span>Name of sender:</span><input type="text"/></li>
+                                <li><span>Telephone of sender:</span><input type="text"/></li>
+                                <li><span>Name of the person voucher to be posted to:</span><input type="text"/></li>
+                                <li><span>Address of where the voucher is to be posted:</span><input type="text"/></li>
+                                <li><span>Message from sender:</span><textarea></textarea></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="step step-2">
+                        <header>
+                            <span>Step 2</span>
+                            <h3>Tickets</h3>
+                            <p>Please select the number of people joining the experience.</p>
+                        </header>
+                        <div class="buy-content">
+                                <div class="tickets-select">
+                                    <div>
+                                       <h5>Ticket type</h5>
+                                       <ol>
+                                           <li>Adult (usually £42.00):</li>
+                                           <li>Child (10 - 14):</li>
+                                           <li>Couple:</li>
+                                           <li>Family (2+2):</li>
+                                       </ol>
+                                   </div>
+                                    <div>
+                                        <h5>Quantity</h5>
+                                        <div>
+                                            <label for="adult"></label>
+                                            <select id="adult" class="cs-select cs-skin-elastic">
+                                                <option value="0" disabled selected>0</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <label for="child"></label>
+                                            <select id="child" class="cs-select cs-skin-elastic">
+                                                <option value="0" disabled selected>0</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <label for="couple"></label>
+                                            <select id="couple" class="cs-select cs-skin-elastic">
+                                                <option value="0" disabled selected>0</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <label for="family"></label>
+                                            <select id="family" class="cs-select cs-skin-elastic">
+                                                <option value="0" disabled selected>0</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                        </div>
+
+
+                                    </div>
+                                    <div>
+                                        <h5>Price(each)</h5>
+                                        <ol>
+                                           <li>£39.95</li>
+                                           <li>£31.50</li>
+                                           <li>£75.00</li>
+                                           <li>£135.00</li>
+                                       </ol>
+                                    </div>
+                                </div>
+                                <div id="thecharter">
+                                       <div>
+                                            <h5>Terms</h5>
+                                            <label for="terms"></label>
+                                            <p>Please confirm that you have read <a href="#">TERMS & CONDITIONS.</a></p>
+                                        </div>
+                                        <div class="chartercheckbox"><input type="checkbox" id="terms"/>Accept</div>
+                                    </div>
+                        </div>
+
+                    </div>
+
+                    <div class="step step-3">
+                        <header>
+                            <span>Step 3</span>
+                            <h3>Re-check  your information and proceed to payment</h3>
+                            <p>You will be redirected to Protx.com, a provider of secure online credit card and debit card payment solutions for thousands of online and mail order businesses across the UK.</p>
+                        </header>
+                        <div class="buy-content">
+                            <div id="resume" class="grid">
+                                <ul class="personal col-1-3">
+                                    <li>Sender</li>
+                                    <li><span>Name</span>Mark Jacob</li>
+                                    <li><span>Phone</span>011 - 15 - 5315 - 9223</li>
+                                    <li><span>E-Mail</span>markjacob@gmail.com</li>
+                                    <li><span>Message</span>Querido Juancito Snow, te envio este voucher para que navegues las aguas del rio Thamesis a toda velocidad. Saludos, tu amigo Mark.</li>
+                                </ul>
+
+                                <ul class="trip col-1-3">
+                                    <li>Receiver</li>
+
+                                    <li><span>Name</span>Jhon Snow</li>
+                                    <li><span>Phone</span>011 - 15 - 3415 - 4423</li>
+                                    <li><span>Address</span>youknownothing@gmail.com</li>
+                                    <li><span>Person of who it is to be posted</span>Jhon</li>
+                                    
+                                </ul>
+
+                                <ul class="extras col-1-3">
+                                    <li>Trip</li>
+                                    <li><span>Trip</span>The Ultimate London Adventure</li>
+                                    <li><span>Adults</span>£75.90 (2)</li>
+                                    <li><span>Childrens</span>£75.90 (2)</li>
+                                    <li><span>Extras</span>£3.95</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <footer>
+                        <p class="msj error"><span class="icon">Error. Please check your information and try again. </p>
+                        <div class="grid">
+                            <div class="col-1-3"><a href="#" id="prev" class="btn md-close btn-secondary"> Cancel </a></div>
+                            <div class="col-1-3"><p class="price-total"><span>$</span><span class="number">0.00</span></p></div>
+                            <div class="col-1-3"><a href="#" id="next" class="btn btn-main btn-next"> Next </a></div>
+                        </div>
+                    </footer>
+                    
+                    <div id="buyhelp">
+                         <div>
+                             <span>Need help with your booking?</span> Call us on: 0207 928 8933
+                         </div>
+                         <div>
+                            <span>*</span>Child Tickets:  14 years & under
+                         </div>
+                    </div>
+
+                </section>
+    </div>
+</div>
+
+
