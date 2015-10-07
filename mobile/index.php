b6dcc83038b0717d0121c4b745db746248726854
César Pantuso <cesar.pantuso@gmail.com>
2015-10-07 11:10:13 -0300

    Missing files

---
+++ mobile/index.php
@@ -0,0 +1,298 @@
+<?php include 'head.php';?>
+
+        <div id="homepage" class="page-content">
+
+        <!-- <video width="100%" height="auto" controls>
+            <source src="content/video.mp4" type="video/mp4" />
+            <source src="content/video.ogv" type="video/ogg" />
+        </video> -->
+
+        <div id="h1-home" >
+            <img src="content/home/h1.png" alt="The fastest, funniest and craziest trip ever !">
+            <h1 class="hidden">The fastest, funniest and craziest trip ever !</h1>
+        </div>
+        <div class="mdl-grid">
+            
+
+            <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-ultimatelondon.jpg" alt="ultimate london adventure" />
+              <div class="mdl-card__supporting-text info">
+                <span>The London Eye  <span class="arrow-mini">></span>   Canary Wharf <span class="arrow-mini">></span>  The London Eye </span>
+                <h2>The Ultimate London Adventure</h2>
+                <p>This is the one that started it all! <br>Our ‘Ultimate London Adventure’ is a jam-packed, fun-filled adrenaline-fuelled 50 minutes.</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>50</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>22</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-ultimatelondon.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=9&subpage=step1&mobile=y" target="_blank"  class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-beware-the-barrier.jpg" alt="Beware the barrier" />
+              <div class="mdl-card__supporting-text info">
+                <span>St Katharine Pier  <span class="arrow-mini">></span>   Thames Barrier <span class="arrow-mini">></span>  St Katharine Pier </span>
+                <h2>Beware the barrier</h2>
+                <p>London RIB Voyages is proud to partner with renowned London Walks to offer you a truly speedy and spooky adventure!</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>40</strong><br>
+                            min. ride + Jack the Ripper Walking Tour
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>8</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-beware-the-barrier.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=29&subpage=step1&mobile=y" target="_blank"  class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <!--Card -->
+            <!-- <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-totallythames.jpg" alt="Totally Thames Blast 2015" />
+              <div class="mdl-card__supporting-text info">
+                <span>St Katharine Pier  <span class="arrow-mini">></span>   Canary Wharf <span class="arrow-mini">></span>  St Katharine Pier </span>
+                <h2>Totally Thames Blast 2015</h2>
+                <p>Celebrate this year’s ‘Totally Thames Festival’ on a river trip with a modern twist !</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>15</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>5</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-totally-thames-blast.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=28&subpage=step1&mobile=y" target="_blank"  class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div> -->
+
+
+             <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-thamesbarrier.jpg" alt="Thames Barrier Explorers Voyage" />
+              <div class="mdl-card__supporting-text info">
+                <span>The London Eye <span class="arrow-mini">></span> Thames Barrier <span class="arrow-mini">></span> The London Eye</span>
+                <h2>Thames Barrier Explorers Voyage</h2>
+                <p> Your journey will take in every major sight the Thames has to offer from the power of Parliament to the might of the Thames Barrier and back. </p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>80’</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>28</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-thamesbarrierexplorersvoyage.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=4&subpage=step1&mobile=y" target="_blank" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+             <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-breakbarrier.jpg" alt="break the barrier" />
+              <div class="mdl-card__supporting-text info">
+                <span>St Katharine Pier <span class="arrow-mini">></span>  Thames Barrier <span class="arrow-mini">></span>  St Katharine Pier</span>
+                <h2>Break the barrier (Speed only)</h2>
+                <p> It’s Fast, Furious and a World First. Thames Rockets is a new thrilling, high-speed boat ride blasting off this summer in the Capital.</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>40’</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>8</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-breakthebarrier.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=12&subpage=step1&mobile=y" target="_blank" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+             <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-captainkidd.jpg" alt="Captain Kidd's Canary Wharf Voyage" />
+              <div class="mdl-card__supporting-text info">
+                <span>The London Eye <span class="arrow-mini">></span> Canary Wharf <span class="arrow-mini">></span> The London Eye </span>
+                <h2>Captain Kidd's Canary Wharf Voyage</h2>
+                <p>Specially designed for families.<br>You can still expect all the thrills and spills that have become the trademark our experience!</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>50’</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>22</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-captainkiddcanarywharf.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=1&subpage=step1&mobile=y" target="_blank" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+             <!--Card -->
+            <div class="mdl-card mdl-shadow--2dp demo-card-square">
+                <img src="content/home/thumb-limehouse.jpg" alt="limehouse blast" />
+              <div class="mdl-card__supporting-text info">
+                <span>The London Eye <span class="arrow-mini">></span> Canary Wharf <span class="arrow-mini">></span> Limehouse Pontoon</span>
+                <h2>Limehouse<br>Blast</h2>
+                <p> London RIB Voyages invite you to start your weekend in style. This is the classiest, tastiest and speediest Friday night in London!</p>
+
+              </div>
+              <div class="mdl-card__actions mdl-card--border acts mdl-grid facts">
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>30’</strong><br>
+                            min. ride
+                        </div>
+                        <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                            <strong>22</strong><br>
+                            sitepoints
+                        </div>
+                        <div class="mdl-cell mdl-cell--1-col mdl-cell--top">
+                            <strong>35</strong><br>
+                            mph<br>
+                        </div>
+               </div>
+               <div class="mdl-card__actions mdl-card--border acts mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="detail-limehouseblast.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      view detail
+                    </a> 
+                </div>
+               
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--top">
+                    <a href="http://www.londonribvoyages.com/booking.php?tour_id=27&subpage=step1&mobile=y" target="_blank" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                      book now
+                    </a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+        </div>
+<?php include 'footer.php';?>
